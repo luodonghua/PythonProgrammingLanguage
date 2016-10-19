38658facba945a9ef86f75d4a31b8cbce9ea9123
luodonghua <luodonghua@gmail.com>
2016-10-19 19:42:43 +0800

    Module4

--- 4.3 Project Handling Bad data with Exception Handling.py
+++ 4.3 Project Handling Bad data with Exception Handling.py
@@ -18,7 +18,7 @@ def csvreader_example(filename):
                     row[5] = float(row[5])
                     row[6] = float(row[6])
                     row[7] = datetime.datetime.strptime(row[7], '%Y-%m-%d')
-                except ValueError as err: # Exception is the top level exception to catch all
+                except ValueError as err: # Exception is the top level exception to catch all (dangerous)
                     print('Row:', rowno, 'Bad row:', row)
                     print('Row:', rowno, 'Reason:', err)
                     continue    # skip to the next line
