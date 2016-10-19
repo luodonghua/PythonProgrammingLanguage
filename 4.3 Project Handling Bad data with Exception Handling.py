dcf93bcbdf78b144a41b58b4c5e5cee630dff5d4
luodonghua <luodonghua@gmail.com>
2016-10-19 19:40:03 +0800

    Module4

---
+++ 4.3 Project Handling Bad data with Exception Handling.py
@@ -0,0 +1,28 @@
+import datetime
+import csv
+
+def csvreader_example(filename):
+    '''
+    :param filename:
+    :return total
+    Example to demonstrate functions
+    '''
+    total = 0.0
+    with open(filename,'r') as f:
+            rows = csv.reader(f)
+            headers = next(rows)           # Skill a single line input
+            rowno = 0
+            for row in rows:
+                rowno += 1  # alternatively use enumerate: for rowno, row in enumerate(rows, start=1)
+                try:
+                    row[5] = float(row[5])
+                    row[6] = float(row[6])
+                    row[7] = datetime.datetime.strptime(row[7], '%Y-%m-%d')
+                except ValueError as err:
+                    print('Row:', rowno, 'Bad row:', row)
+                    print('Row:', rowno, 'Reason:', err)
+                    continue    # skip to the next line
+                total += row[5]
+    return total
+
+print('Total is:', csvreader_example('airports.csv'))
